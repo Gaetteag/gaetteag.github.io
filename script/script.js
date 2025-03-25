b26ae9f1a076ab96f55c256af4eda2c087346c3a
Gaëtan <eag53@hotmail.fr>
2025-03-25 10:30:08 +0100

    Ajout d'une partie "universe"

--- script/script.js
+++ script/script.js
@@ -109,8 +109,54 @@ function createPortfolioFromJSON() {
         });
 }
 
+// Function to dynamically create HTML elements from the JSON file
+function createUniverseFromJSON() {
+    const container = document.querySelector("#universe .container");
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    // Load the JSON file
+    fetch("data/universe.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Iterate through the JSON data and create HTML elements
+            data.forEach((item, index) => {
+                const card = document.createElement("div");
+                card.classList.add("col-lg-4", "mt-4");
+                const points = item.text.split(", ");
+                const ul = document.createElement("ul");
+                points.forEach((point) => {
+                    const li = document.createElement("li");
+                    li.textContent = point.trim();
+                    ul.appendChild(li);
+                });
+
+                // **Ajout du 'ul' dans la carte à la place du 'p'**
+                card.innerHTML = `
+                    <div class="card universeText">
+                        <div class="card-body">
+                            <h3 class="card-title mt-3">${item.title}</h3>
+                        </div>
+                    </div>
+                `;
+                card.querySelector(".card-body").appendChild(ul)
+
+                // Append the card to the current row
+                row.appendChild(card);
+
+                // If the index is a multiple of 3 or it's the last element, create a new row
+                if ((index + 1) % 3 === 0 || index === data.length - 1) {
+                    container.appendChild(row);
+                    row = document.createElement("div");
+                    row.classList.add("row");
+                }
+            });
+        });
+}
+
 // Call the functions to execute the code
 handleNavbarScroll();
 handleNavbarCollapse();
 createSkillsFromJSON();
 createPortfolioFromJSON();
+createUniverseFromJSON();
